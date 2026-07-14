1a12ad863c079de3b3687d47b032664be2b4dda2
bruceathy <67832593+bruceathy@users.noreply.github.com>
2026-07-13 23:32:51 -0400

    type vs interface

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,38 @@
-console.log("Hello, Typescript!");
+type Person = {
+  name: string;
+  age: number;
+  isDeveloper?: boolean;
+  address: {
+    street: string;
+    city: string;
+    country: string;
+    zipCode: number;
+  };
+};
 
-let a: number = 3;
+const person: Person = {
+  name: "John Doe",
+  age: 30,
+  isDeveloper: true,
+  address: {
+    street: "123 Main St",
+    city: "Anytown",
+    country: "USA",
+    zipCode: 12345,
+  },
+};
+
+const person2: Person = {
+  name: "Jane Doe",
+  age: 27,
+  address: {
+    street: "123 Main St",
+    city: "Anytown",
+    country: "USA",
+    zipCode: 12345,
+  },
+};
+
+console.log(
+  `this is typescript practice for ${person.name} and ${person2.name} who is ${person.age} and ${person2.age} years old`,
+);
