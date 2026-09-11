1060831593e58a2f7ae3e632c47bcbb53cad96cb
bruceathy <67832593+bruceathy@users.noreply.github.com>
2026-09-10 23:59:31 -0400

    up

--- src/main.ts
+++ src/main.ts
@@ -1,52 +1,10 @@
 type Person = {
   name: string;
-  age: number;
-  isDeveloper?: boolean;
-  address: {
-    street: string;
-    city: string;
-    country: string;
-    zipCode: number;
-  };
+  skillLevel: "beginner" | "intermediate" | "expert";
+  yearsOfExperience?: number;
 };
-
-function greet(name: string, age: number) {
-  console.log(`Hello, ${name}! You are ${age} years old.`);
-}
 
 const person: Person = {
   name: "John Doe",
-  age: 30,
-  isDeveloper: true,
-  address: {
-    street: "123 Main St",
-    city: "Anytown",
-    country: "USA",
-    zipCode: 12345,
-  },
+  skillLevel: "expert",
 };
-
-const person2: Person = {
-  name: "Jane Doe",
-  age: 27,
-  address: {
-    street: "123 Main St",
-    city: "Anytown",
-    country: "USA",
-    zipCode: 12345,
-  },
-};
-
-greet(person.name, person.age);
-console.log(
-  `this is typescript practice for ${person.name} and
-${person2.name} who is ${person.age} and ${person2.age} years old`,
-);
-
-// optional parameters
-function printNameAndAge(name: string, age?: number) {
-  console.log(`Name: ${name}, Age: ${age || "N/A"} years old`);
-}
-
-printNameAndAge("garf");
-printNameAndAge(person2.name);
